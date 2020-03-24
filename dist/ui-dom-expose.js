81cfc5caca66daf95e451afffd6c027df678c4ae
Hamidreza <hramini72@gmail.com>
2020-03-24 17:27:55 +0430

    adding ui-dom-expose.js

---
+++ dist/ui-dom-expose.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dom_frame_builder_class_1 = require("./builder/dom-frame-builder-class");
+exports.DomFrameBuilder = dom_frame_builder_class_1.DomFrameBuilder;
+var dom_tag_builder_class_1 = require("./builder/dom-tag-builder-class");
+exports.DomTagBuilder = dom_tag_builder_class_1.DomTagBuilder;
+var dom_primer_class_1 = require("./primer/dom-primer-class");
+exports.DomPrimer = dom_primer_class_1.DomPrimer;
